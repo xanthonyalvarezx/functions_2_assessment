0a566778d54c97b8aa1ec6ff3d3d9b230eda05af
AntMan <xanthonyalvarezx@gmail.com>
2020-05-21 20:00:16 -0400

    got power to almost work

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ console.log(add(2, 4))
 //Multiplication
 function multiply(a, b) { // function which multiplies a,b which = 5,3
 
-  for (i = 0; i < b; i++) { // for i = 0 when < b which is 3 incriment i by 1
+  let total = 0 // total = 0
+  for (let i = 0; i < b; i++) { // for i = 0 when < b which is 3 incriment i by 1
     total = add(a, total) // add a which is 5 to the total which is 0 making it 5 then 10 then 15
   }
+  console.log(total) // console log the total which should be 5 * 3 = 15
   return total
 }
-let total = 0 // total = 0
 multiply(5, 3) // multiply 5 and 3
-console.log(total) // console log the total which should be 5 * 3 = 15
 
 //Power/Exponent
 
 function power(x, n) {
   let result = 1
-  for (i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     result = multiply(x, result)
   }
   console.log(result)
